31fb6fd256978fad8cac1fe7df7b4f1a720e6b86
Yogesh Choudhary <yogesh986.be22@chitkara.edu.in>
2024-08-27 23:30:05 +0530

    Starting project by creating login and register page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Login from './components/login'
+import Register from './components/register'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <Register />
+    </>
+  )
+}
+
+export default App
